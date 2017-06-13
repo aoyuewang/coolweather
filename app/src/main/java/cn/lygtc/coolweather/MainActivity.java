ac6ca54f45439fe915a22067f77ffb26c1681748
aoyuewang <mokeasl@look.com>
2017-06-13 11:14:55 +0800

    first commit

--- app/src/main/java/cn/lygtc/coolweather/MainActivity.java
+++ app/src/main/java/cn/lygtc/coolweather/MainActivity.java
@@ -2,9 +2,17 @@ package cn.lygtc.coolweather;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.TextureView;
+import android.widget.TextView;
+
+import com.baidu.location.LocationClient;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
-
+    public LocationClient mLocationClient;
+    private TextView positionText;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
